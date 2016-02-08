9dd912935769ea5b288e6a7f24fe63a71f0d60dd
Matthias Kretschmann <m@kretschmann.io>
2016-02-08 18:06:21 +0100

    new SVG-based graph with subtle intro animation

--- _src/_assets/javascripts/page-front.js
+++ _src/_assets/javascripts/page-front.js
@@ -1,2 +1,19 @@
 
 //=include bigchain/smoothscroll.js
+//=include ../../../node_modules/vivus/dist/vivus.js
+
+
+jQuery(function($) {
+
+    // graph animation
+    var iconOptions = {
+        type: 'delayed',
+        start: 'inViewport',
+        duration: 200,
+        animTimingFunction: Vivus.EASE_OUT,
+        selfDestroy: true
+    };
+
+    new Vivus('bigchain-graph', iconOptions);
+
+});
