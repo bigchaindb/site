ac3e91927fb1ff1e673838d6c9757f9f014fde04
Matthias Kretschmann <m@kretschmann.io>
2016-02-11 16:45:32 +0100

    make Wrigely wink reliably and do more stuff, fixes #50

--- _src/_assets/javascripts/page-community.js
+++ _src/_assets/javascripts/page-community.js
@@ -3,13 +3,45 @@
 
 jQuery(function($) {
 
-    // start wrigely animation when in viewport
-    var wrigely = $('#wrigely');
+    var wrigely     = $('#wrigely'),
+        wrigelyArm  = wrigely.find('#arm'),
+        wrigelyHead = wrigely.find('#head'),
+        wrigelyEye  = wrigely.find('#eye');
 
-    if (wrigely.isInViewport() ) {
-        wrigely.addClass('is-ready');
-    } else {
-        wrigely.removeClass('is-ready');
+    var wrigelyAnimationStart = function() {
+
+        // set utility classes
+        if (wrigely.is( ':in-viewport( -180 )' ) ) {
+            wrigely.addClass('is-ready');
+
+            // toggle paused class so the :hover interaction can fire animation again
+            wrigelyArm.on('animationend webkitAnimationEnd oAnimationEnd', function(e) {
+                    wrigelyArm.addClass('paused');
+                    wrigelyHead.addClass('paused');
+
+                    wrigely.on('mouseover', function() {
+                        wrigelyArm.removeClass('paused');
+                        wrigelyHead.removeClass('paused');
+                    });
+
+                    // fire eye blinking once other animations have finished
+                    wrigelyEye.addClass('is-ready');
+
+                    wrigelyEye.on('animationend webkitAnimationEnd oAnimationEnd', function(e) {
+                        wrigelyEye.removeClass('is-ready');
+                    });
+                }
+            );
+        } else {
+            wrigely.removeClass('is-ready').removeClass('paused');
+            wrigelyArm.removeClass('paused');
+            wrigelyHead.removeClass('paused');
+            wrigelyEye.removeClass('is-ready').removeClass('paused');
+        }
     }
+
+    // fire all the viewport things
+    wrigelyAnimationStart();
+    $(window).on('load resize scroll', wrigelyAnimationStart);
 
 });
