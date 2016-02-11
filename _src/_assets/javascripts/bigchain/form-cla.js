c9cab37ee7b9508982c902a82834cbd2daab4509
Matthias Kretschmann <m@kretschmann.io>
2016-02-11 03:11:43 +0100

    lots of CLA form interaction improvements, fixes #44
    - disable submit button unless checkbox has been clicked
    - better account for multiple forms: after sending one form other form is still visible
    - add custom subject to differentiate between both forms
    - fix for parsley.js not validating checkbox
    - fix entity form elements not selectable
    - form spacing tweaks

--- _src/_assets/javascripts/bigchain/form-cla.js
+++ _src/_assets/javascripts/bigchain/form-cla.js
@@ -6,36 +6,55 @@ var FormCla = (function(w, d, $) {
     var app, _private, _config;
 
     _config = {
-        form:       $('.form-cla'),
-        formBtn:    $('.form-cla').find('.btn'),
-        formURL:    $('.form-cla').attr('action'),
-        formMethod: $('.form-cla').attr('method')
+        form:         $('.form-cla'),
+        formCheckbox: $('.form-cla').find('.agree'),
+        formBtn:      $('.form-cla').find('.btn'),
+        formURL:      $('.form-cla').attr('action'),
+        formMethod:   $('.form-cla').attr('method')
     };
 
     _private = {
+        formAgree: function() {
+
+            // disable submit button by default
+            _config.formBtn.addClass('disabled').attr('disabled', 'disabled');
+
+            // toggle submit button state based on checkbox click
+            _config.formCheckbox.on('click', function(){
+                if ( $(this).is(':checked') ) {
+                    _config.formBtn.removeClass('disabled').removeAttr('disabled');
+                } else {
+                    _config.formBtn.addClass('disabled').attr('disabled', 'disabled');
+                }
+            });
+        },
+
         formSubmit: function() {
             _config.form.submit(function(e) {
                 e.preventDefault();
 
-                if ( $(this).parsley().isValid() ) {
+                var thisForm   = $(this),
+                    thisButton = thisForm.find('.btn');
+
+                if ( thisForm.parsley().isValid() ) {
                     $.ajax({
                         url: _config.formURL,
                         method: _config.formMethod,
-                        data: $(this).serialize(),
+                        data: thisForm.serialize(),
                         dataType: 'json',
                         beforeSend: function() {
-                            _config.formBtn
+                            thisButton
                                 .addClass('disabled')
                                 .attr('value', 'Sending...');
                         },
                         success: function(data) {
-                            _config.form.find('.form-group').addClass('hide');
-                            _config.form.find('.alert-success').removeClass('hide');
-                            _config.formBtn.removeClass('disabled');
+                            thisForm.find('.form-group').addClass('hide');
+                            thisForm.find('.alert-success').removeClass('hide');
+                            thisButton.removeClass('disabled');
                         },
                         error: function(err) {
-                            _config.form.find('.alert-danger').removeClass('hide');
-                            _config.formBtn
+                            thisForm.find('.alert-danger').removeClass('hide');
+                            thisButton
                                 .removeClass('disabled')
                                 .attr('value', 'Send');
                         }
@@ -48,6 +67,7 @@ var FormCla = (function(w, d, $) {
     app = {
         init: function() {
             _private.formSubmit();
+            _private.formAgree();
         }
     };
 
