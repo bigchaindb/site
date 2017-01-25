2919c48035ea382e982895699ea3a70bc1d1d6dd
Matthias Kretschmann <m@kretschmann.io>
2017-01-25 13:21:36 +0100

    quickfix: remove hero video (#92)
    * addressing #91

--- _src/_assets/javascripts/page-front.js
+++ _src/_assets/javascripts/page-front.js
@@ -1,13 +1,10 @@
 
-//=include bigchain/hero-video.js
 //=include bigchain/smoothscroll.js
 //=include bigchain/testimonials.js
 
 jQuery(function($) {
 
     // hero stuff
-    HeroVideo.init();
-
     $('.hero .logo').on('animationend webkitAnimationEnd oAnimationEnd',
         function(e) {
             $('.hero').addClass('is-ready');
