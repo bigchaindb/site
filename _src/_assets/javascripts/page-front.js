8386d9692b4d9a784912a4ef88aa6fa49c13aa4b
Matthias Kretschmann <m@kretschmann.io>
2017-03-02 14:04:29 +0100

    hero elements & markup cleanup

--- _src/_assets/javascripts/page-front.js
+++ _src/_assets/javascripts/page-front.js
@@ -4,13 +4,6 @@
 
 jQuery(function($) {
 
-    // hero stuff
-    $('.hero .logo').on('animationend webkitAnimationEnd oAnimationEnd',
-        function(e) {
-            $('.hero').addClass('is-ready');
-        }
-    );
-
     Testimonials.init();
 
 });
