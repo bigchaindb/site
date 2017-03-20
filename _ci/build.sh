8103c6884e9d0855337f4dd7cb19b15208b59dfe
Matthias Kretschmann <m@kretschmann.io>
2017-03-20 15:46:55 +0100

    prevent search engine indexing of beta & gamma

--- _ci/build.sh
+++ _ci/build.sh
@@ -8,7 +8,32 @@ echo "              Starting build "
 echo "============================================="
 echo "$(tput sgr0)" # reset
 
+##
+## check for pull request against master
+##
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+
+    export JEKYLL_ENV=betadeploy
+
     gulp build --production
+
+
+##
+## check for master push which is no pull request
+##
+elif [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+
+    export JEKYLL_ENV=livedeploy
+
+    gulp build --production
+
+else
+
+    gulp build --production
+
+fi;
+
+
 
 echo "$(tput setaf 64)" # green
 echo "---------------------------------------------"
