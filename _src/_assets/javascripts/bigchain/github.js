3bb0e8e129e08ee608f3c25ef808c74741a88253
Matthias Kretschmann <m@kretschmann.io>
2017-05-06 22:09:00 +0200

    fix build

--- _src/_assets/javascripts/bigchain/github.js
+++ _src/_assets/javascripts/bigchain/github.js
@@ -1,10 +1,10 @@
 (function() {
 
-    const s = document.createElement('script'),
-          t = document.createElement('script'),
-          githubApiUrl = 'https://api.github.com/repos/',
-          owner = 'bigchaindb',
-          repo = 'bigchaindb';
+    var s = document.createElement('script'),
+        t = document.createElement('script'),
+        githubApiUrl = 'https://api.github.com/repos/',
+        owner = 'bigchaindb',
+        repo = 'bigchaindb';
 
     s.async = true;
     s.src = githubApiUrl + owner + '/' + repo + '?callback=' + owner + '.getGitHubRepoInfo';
@@ -13,14 +13,14 @@
     t.src = githubApiUrl + owner + '/' + repo + '/releases/latest?callback=' + owner + '.getGitHubReleaseInfo';
 
     window[owner] = window[owner] || {};
-    window[owner].getGitHubRepoInfo = (response) => {
+    window[owner].getGitHubRepoInfo = function(response) {
 
         var stargazers = response.data.stargazers_count;
 
         document.getElementById('stargazers').innerText = stargazers;
     };
 
-    window[owner].getGitHubReleaseInfo = (response) => {
+    window[owner].getGitHubReleaseInfo = function(response) {
 
         var version = response.data.tag_name;
 
