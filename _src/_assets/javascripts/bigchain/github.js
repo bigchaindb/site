6c095a14a604396107e2a53e3866367022b1a75a
Matthias Kretschmann <m@kretschmann.io>
2017-05-06 18:41:04 +0200

    grab latest release stuff from GitHub API too

--- _src/_assets/javascripts/bigchain/github.js
+++ _src/_assets/javascripts/bigchain/github.js
@@ -1,21 +1,32 @@
 (function() {
 
-    var s = document.createElement('script'),
-        githubApiUrl = 'https://api.github.com/repos/',
-        owner = 'bigchaindb',
-        repo = 'bigchaindb';
+    const s = document.createElement('script'),
+          t = document.createElement('script'),
+          githubApiUrl = 'https://api.github.com/repos/',
+          owner = 'bigchaindb',
+          repo = 'bigchaindb';
 
-    s.type = 'text/javascript';
     s.async = true;
     s.src = githubApiUrl + owner + '/' + repo + '?callback=' + owner + '.getGitHubRepoInfo';
 
+    t.async = true;
+    t.src = githubApiUrl + owner + '/' + repo + '/releases/latest?callback=' + owner + '.getGitHubReleaseInfo';
+
     window[owner] = window[owner] || {};
-    window[owner].getGitHubRepoInfo = function(response) {
+    window[owner].getGitHubRepoInfo = (response) => {
 
         var stargazers = response.data.stargazers_count;
 
         document.getElementById('stargazers').innerText = stargazers;
     };
 
+    window[owner].getGitHubReleaseInfo = (response) => {
+
+        var version = response.data.tag_name;
+
+        document.getElementById('version').innerText = version;
+    };
+
     document.getElementsByTagName('HEAD')[0].appendChild(s);
+    document.getElementsByTagName('HEAD')[0].appendChild(t);
 }());
