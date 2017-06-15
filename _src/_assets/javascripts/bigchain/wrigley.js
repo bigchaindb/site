e4d00cb16b0cd6de75cc383315e74a34ef099354
Matthias Kretschmann <m@kretschmann.io>
2017-06-15 21:53:50 +0200

    wrigley.js

---
+++ _src/_assets/javascripts/bigchain/wrigley.js
@@ -0,0 +1,48 @@
+//=include is-in-viewport/lib/isInViewport.js
+
+jQuery(function($) {
+
+    //
+    // Wrigley
+    //
+    var wrigley     = $('#wrigley'),
+        wrigleyArm  = wrigley.find('#arm'),
+        wrigleyHead = wrigley.find('#head'),
+        wrigleyEye  = wrigley.find('#eye')
+
+    var wrigleyAnimationStart = function() {
+
+        // set utility classes
+        if (wrigley.is( ':in-viewport( -180 )' ) ) {
+            wrigley.addClass('is-ready')
+
+            // toggle paused class so the :hover interaction can fire animation again
+            wrigleyArm.on('animationend webkitAnimationEnd oAnimationEnd', function(e) {
+                    wrigleyArm.addClass('paused')
+                    wrigleyHead.addClass('paused')
+
+                    wrigley.on('mouseover', function() {
+                        wrigleyArm.removeClass('paused')
+                        wrigleyHead.removeClass('paused')
+                    })
+
+                    // fire eye blinking once other animations have finished
+                    wrigleyEye.addClass('is-ready')
+
+                    wrigleyEye.on('animationend webkitAnimationEnd oAnimationEnd', function(e) {
+                        wrigleyEye.removeClass('is-ready')
+                    })
+                }
+            )
+        } else {
+            wrigley.removeClass('is-ready').removeClass('paused')
+            wrigleyArm.removeClass('paused')
+            wrigleyHead.removeClass('paused')
+            wrigleyEye.removeClass('is-ready').removeClass('paused')
+        }
+    }
+
+    // fire all the viewport things
+    wrigleyAnimationStart()
+    $(window).on('load resize scroll', wrigleyAnimationStart)
+})
--- _src/_assets/javascripts/page-getstarted.js
+++ _src/_assets/javascripts/page-getstarted.js
@@ -1,6 +1,6 @@
 //=include bigchain/smoothscroll.js
 //=include bigchain/newsletter.js
-//=include is-in-viewport/lib/isInViewport.js
+//=include bigchain/wrigley.js
 
 jQuery(function($) {
 
@@ -8,49 +8,4 @@ jQuery(function($) {
     // init modules
     //
     Newsletter.init()
-
-
-    //
-    // Wrigley
-    //
-    var wrigley     = $('#wrigley'),
-        wrigleyArm  = wrigley.find('#arm'),
-        wrigleyHead = wrigley.find('#head'),
-        wrigleyEye  = wrigley.find('#eye')
-
-    var wrigleyAnimationStart = function() {
-
-        // set utility classes
-        if (wrigley.is( ':in-viewport( -180 )' ) ) {
-            wrigley.addClass('is-ready')
-
-            // toggle paused class so the :hover interaction can fire animation again
-            wrigleyArm.on('animationend webkitAnimationEnd oAnimationEnd', function(e) {
-                    wrigleyArm.addClass('paused')
-                    wrigleyHead.addClass('paused')
-
-                    wrigley.on('mouseover', function() {
-                        wrigleyArm.removeClass('paused')
-                        wrigleyHead.removeClass('paused')
-                    })
-
-                    // fire eye blinking once other animations have finished
-                    wrigleyEye.addClass('is-ready')
-
-                    wrigleyEye.on('animationend webkitAnimationEnd oAnimationEnd', function(e) {
-                        wrigleyEye.removeClass('is-ready')
-                    })
-                }
-            )
-        } else {
-            wrigley.removeClass('is-ready').removeClass('paused')
-            wrigleyArm.removeClass('paused')
-            wrigleyHead.removeClass('paused')
-            wrigleyEye.removeClass('is-ready').removeClass('paused')
-        }
-    }
-
-    // fire all the viewport things
-    wrigleyAnimationStart()
-    $(window).on('load resize scroll', wrigleyAnimationStart)
 })
