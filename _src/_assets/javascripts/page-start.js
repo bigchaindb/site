fe3e1a192ad9cb7a3203fdcb6a9306ef6c0a4d32
Matthias Kretschmann <m@kretschmann.io>
2017-06-15 18:54:34 +0200

    drop da base

---
+++ _src/_assets/javascripts/page-start.js
@@ -0,0 +1,14 @@
+
+//=include bigchain/smoothscroll.js
+//=include bigchain/github.js
+//=include bigchain/meetup.js
+//=include bigchain/newsletter.js
+
+jQuery(function($) {
+
+    //
+    // init modules
+    //
+    Newsletter.init()
+
+});
