d3e511a33fa26262f2ba7a122278098b093ab890
Matthias Kretschmann <m@kretschmann.io>
2017-06-22 17:23:48 +0200

    prepare transaction js

--- _src/_assets/javascripts/page-getstarted.js
+++ _src/_assets/javascripts/page-getstarted.js
@@ -9,27 +9,27 @@ jQuery(function($) {
     Newsletter.init()
 })
 
-window.addEventListener('load', function load(event){
-    window.removeEventListener('load', load, false)
+//=include bigchaindb-driver/dist/browser/bundle.window.min.js
+
+window.addEventListener('DOMContentLoaded', function domload(event){
+    window.removeEventListener('DOMContentLoaded', domload, false)
 
     const driver = window.BigchainDB
     const API_PATH = 'https://test.ipdb.io/api/v1/'
     const APP_ID = 'b563bf22'
     const APP_KEY = 'fd639614dcf8ee90a8c51a013ac11fb0'
 
+    const form = document.getElementById('form-transaction')
     const postButton = document.getElementById('post')
 
     postButton.addEventListener('click', function(e) {
         e.preventDefault()
 
-        const alice = new driver.Ed25519Keypair()
-
         const message = document.getElementById('message').value
-        const metadata = document.getElementById('metadata').value
 
+        const alice = new driver.Ed25519Keypair()
         const tx = driver.Transaction.makeCreateTransaction(
             { assetMessage: message },
-            { metaDataMessage: metadata },
             [ driver.Transaction.makeOutput(
                     driver.Transaction.makeEd25519Condition(alice.publicKey))
             ],
