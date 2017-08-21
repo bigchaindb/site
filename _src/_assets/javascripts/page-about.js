3a0b6940971d8054eec280f525fe387973b0c449
Matthias Kretschmann <m@kretschmann.io>
2017-08-22 00:50:41 +0200

    integrate founder bios into about page

---
+++ _src/_assets/javascripts/page-about.js
@@ -0,0 +1,18 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    var trigger = document.getElementsByClassName('team__bio__trigger')[0]
+    var bio = document.getElementsByClassName('team__bio')
+
+    trigger.addEventListener('click', function(e) {
+        for (var i = 0; i < bio.length; ++i) {
+            var item = bio[i]
+            if (item.style.display == 'block') {
+                item.style.display = 'none'
+                trigger.innerHTML = 'Show bios'
+            } else {
+                item.style.display = 'block'
+                trigger.innerHTML = 'Hide bios'
+            }
+        }
+    })
+})
