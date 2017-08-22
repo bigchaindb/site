959d2c51f3a084dccda9f7a153ef449e55cc8f36
Matthias Kretschmann <m@kretschmann.io>
2017-08-23 00:33:46 +0200

    more bio tweaks

--- _src/_assets/javascripts/page-about.js
+++ _src/_assets/javascripts/page-about.js
@@ -6,13 +6,60 @@ document.addEventListener('DOMContentLoaded', function() {
     trigger.addEventListener('click', function(e) {
         for (var i = 0; i < bio.length; ++i) {
             var item = bio[i]
+
             if (item.style.display == 'block') {
                 item.style.display = 'none'
                 trigger.innerHTML = '<span>+</span>Show bios'
+                hide(item)
             } else {
                 item.style.display = 'block'
                 trigger.innerHTML = '<span>−</span>Hide bios'
+                show(item)
             }
         }
     })
 })
+
+function show(ele) {
+    raf = window.requestAnimationFrame(function() {
+        var opacity = 0
+        var height = 0
+
+        function increase () {
+            opacity += 0.1
+            height += 1
+            if (opacity >= 1) {
+                // complete
+                ele.style.opacity = 1
+                ele.style.height = '100%'
+                return true
+            }
+            ele.style.opacity = opacity
+            ele.style.height = height + '%'
+
+            requestAnimationFrame(increase)
+        }
+        increase()
+    });
+}
+
+function hide(ele) {
+    raf = window.requestAnimationFrame(function() {
+        var opacity = 1
+        var height = '100%'
+        function decrease () {
+            opacity += 0.1
+            height += 1
+            if (opacity >= 1) {
+                // complete
+                ele.style.opacity = 0
+                ele.style.height = 0
+                return true
+            }
+            ele.style.opacity = opacity
+            ele.style.height = height + '%'
+            requestAnimationFrame(decrease)
+        }
+        decrease()
+    })
+}
