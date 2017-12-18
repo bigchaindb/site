675aecedef70e0068fba7d3b9d4c4f25b81b95a9
Matthias Kretschmann <m@kretschmann.io>
2017-12-18 21:28:08 +0100

    switch over urls for transaction tool

--- _src/_assets/javascripts/page-getstarted.js
+++ _src/_assets/javascripts/page-getstarted.js
@@ -3,7 +3,8 @@
 //=include bigchain/tab.js
 //=include bigchain/newsletter.js
 
-const ipdbUrl = 'https://test.ipdb.io'
+const bigchaindbUrl = 'https://test.bigchaindb.com'
+const proxyUrl = 'https://getstarted.ipdb.io:4443'
 
 jQuery(function($) {
 
@@ -54,7 +55,7 @@ window.addEventListener('DOMContentLoaded', function domload(event) {
     window.removeEventListener('DOMContentLoaded', domload, false)
 
     const driver = window.BigchainDB
-    const API_PATH = 'https://getstarted.ipdb.io:4443/api/v1/'
+    const API_PATH = proxyUrl + '/api/v1/'
 
     const form = document.getElementById('form-transaction')
     const postButton = document.getElementById('post')
@@ -124,7 +125,7 @@ window.addEventListener('DOMContentLoaded', function domload(event) {
             const outputContent = JSON.stringify(response, null, 2) // indented with 2 spaces
             output.textContent = outputContent
 
-            transactionLink.href = ipdbUrl + '/api/v1/transactions/' + response.id
+            transactionLink.href = bigchaindbUrl + '/api/v1/transactions/' + response.id
 
             postButton.classList.add('disabled')
             postButton.style.opacity = 0
